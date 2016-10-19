6cabac08cc0eaea5a64f7f51b5bdbb2fe2e06b25
bhagya <bhagya@outlook.com>
2016-10-19 12:11:46 -0500

    modified Letter constructor - display function is no put on prototype

--- letter.js
+++ letter.js
@@ -3,12 +3,12 @@ module.exports= function Letter(l){
 this.name = l;
 this.guessed = false;
 
-this.display = function(){
+}
+
+Letter.prototype.display = function(){
 		if(this.guessed === false){
 			return '-';
 		}else{
 			return l;
 		}
 	}
-
-}
